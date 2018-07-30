aac290bacd44ea75e0fb992ca70d2351e269177e
Mike <mikeliao97@berkeley.edu>
2018-07-29 17:24:05 -0700

    add bash scripts for setup

--- vs060_gazebo/bash_scripts/start_gazebo_moveit.sh
+++ vs060_gazebo/bash_scripts/start_gazebo_moveit.sh
@@ -9,13 +9,23 @@ killall -9 rosmaster & killall -9 roscore
 
 
 echo "-----Starting up Gazebo+Denso Controllers ---"
-tmux new-session 
-tmux new-window "roslaunch vs060_gazebo vs060_gazebo_new.launch"
+session_name="gazebo"
 
+#Remove any old tmux sessions  if there is one
+tmux kill-session -t ${session_name}
+
+#Start a new session
+tmux new -d -s ${session_name}
+tmux set-option remain-on-exit on
+
+tmux selectp -t ${session_name}
+tmux split-window -v "source setup && roslaunch vs060_gazebo vs060_gazebo_new.launch"
+
+#Sleep for 3 seconds for gazebo controllers to come up
+echo "---Sleep for 3 seconds ---"
 sleep 3 
 
-tmux split-window "roslaunch vs060_newmoveit_config myrobot_planning_execution.launch"
-
-echo "---Sleep for 3 seconds ---"
-
 echo "---Starting Moveit!---"
+tmux split-window -h "source setup && roslaunch vs060_newmoveit_config myrobot_planning_execution.launch"
+
+tmux attach -t ${session_name}
